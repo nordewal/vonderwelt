ec231246dc47ff316b55496e76a8b414c4092839
Janosch Rohdewald <nordewal@gmail.com>
2017-01-25 22:06:43 +0100

    enumerate over files in directory and prepare gallery

--- lib/custom_helpers.rb
+++ lib/custom_helpers.rb
@@ -4,4 +4,20 @@ module CustomHelpers
     #![Alternative text](images/IMG_0017.JPG "Title attribute"){: srcset="images/IMG_0017.JPG 2x"}
     #return image_path("IMG_0017.JPG", resize_to: 1200)
   end
+
+  def article_images(article)
+    images = []
+    Dir.glob("source/#{File.dirname(article.path)}/*").grep(/^.*(?<!_small|large)\.(jpg|png)$/i) do |item|
+      images << File.basename(item)
+    end
+    images
+  end
+
+  def image_small_path(img_name)
+    image_path(img_name.gsub(/\.(.*)$/, '_small.\1'))
+  end
+
+  def image_large_path(img_name)
+    image_path(img_name.gsub(/\.(.*)$/, '_large.\1'))
+  end
 end
--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -10,31 +10,31 @@ $('div.blog_grid div.column').each(function(i, elem){
 })
 
 
-var pswpElement = document.querySelectorAll('.pswp')[0];
-
-// build items array
-var items = [
-    {
-        src: 'namibia-botswana/PC7C9303_0_small.jpg',
-        w: 600,
-        h: 400
-    },
-    {
-        src: 'namibia-botswana/PC7C7601_0_01_small.jpg',
-        w: 1200,
-        h: 900
-    }
-];
-
-// define options (if needed)
-var options = {
-// optionName: 'option value'
-// for example:
-    index: 0 // start at first slide
-};
-
-// Initializes and opens PhotoSwipe
-var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-gallery.init();
-
-openPhotoSwipe();
+// var pswpElement = document.querySelectorAll('.pswp')[0];
+//
+// // build items array
+// var items = [
+//     {
+//         src: 'namibia-botswana/PC7C9303_0_small.jpg',
+//         w: 600,
+//         h: 400
+//     },
+//     {
+//         src: 'namibia-botswana/PC7C7601_0_01_small.jpg',
+//         w: 1200,
+//         h: 900
+//     }
+// ];
+//
+// // define options (if needed)
+// var options = {
+// // optionName: 'option value'
+// // for example:
+//     index: 0 // start at first slide
+// };
+//
+// // Initializes and opens PhotoSwipe
+// var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+// gallery.init();
+//
+// openPhotoSwipe();
