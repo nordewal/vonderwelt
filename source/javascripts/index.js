95f7024fe85b0bbd20a0f6c6d06f710f475b8b47
Janosch Rohdewald <nordewal@gmail.com>
2017-02-14 20:09:54 +0100

    nice browsing using tags; add service blog

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -9,22 +9,57 @@ function Index() {
     if ($(blogGrid).length == 0)
         return;
 
+    var msnry = null
+
     Foundation.onImagesLoaded($(blogGrid + ' img'), function () {
-        var msnry = new Masonry(blogGrid, {
+        msnry = new Masonry(blogGrid, {
             itemSelector: '.grid-item',
             columnWidth: '.grid-sizer',
             percentPosition: true,
-            transitionDuration: 0
+            transitionDuration: '0.6'
         });
-        msnry.layout();
 
+        showTagged();
         $(blogGrid + ' img').removeClass('init');
-        $('div.blog-grid div.grid-item a').each(function (i, elem) {
-            MotionUI.animateIn(elem, 'zoom-in');
+
+         $('div.blog-grid div.grid-item a').each(function (i, elem) {
+             MotionUI.animateIn(elem, 'zoom-in');
         });
 
-        msnry.options.transitionDuration='0.6s'
+        msnry.options.transitionDuration = '0.6s'
     });
+
+    function getShowHideElements() {
+        var tag = decodeURIComponent(window.location.hash.replace('#', ''));
+
+        var currentlyShown = $('div.blog-grid div.grid-item');
+        var shouldBeShown = $('div.blog-grid div.real-grid-item');
+
+        if (tag != '')
+            shouldBeShown = $('div.blog-grid div.real-grid-item[data-tags*="' + tag + '"]');
+
+        var toShow = shouldBeShown.not(currentlyShown);
+        var toHide = currentlyShown.not(shouldBeShown);
+
+        return {'toShow': toShow, 'toHide': toHide};
+    }
+
+    function showTagged() {
+        var elements = getShowHideElements();
+
+        elements['toHide'].removeClass('grid-item');
+        elements['toShow'].removeAttr('style').addClass('grid-item');
+
+        msnry.hideItemElements(elements['toHide']);
+        msnry.reloadItems();
+        msnry.layout();
+    }
+
+    $(function () {
+        if ($('div.blog-grid').length > 0) {
+            $(window).on('hashchange', showTagged);
+        }
+    })
 }
 
 module.exports = Index
