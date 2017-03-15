d39097faeaeaea051eafe376fd2f9cee7400f1f5
Janosch Rohdewald <nordewal@gmail.com>
2017-03-15 21:49:54 +0100

    set s3 expires/cache-control headers

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
 s3cmd -r --exclude="*small*" --exclude="*large*" put source/blog/ s3://vonderwelt-source
-s3cmd -r -P --exclude=".git" put build/ s3://vonderwelt
+s3cmd -r -P --exclude=".git" --add-header='Cache-Control:max-age=31536000, public' --add-header="Expires:`date -u +"%a, %d %b %Y %H:%M:%S GMT" --date "next Year"`" put build/ s3://vonderwelt
