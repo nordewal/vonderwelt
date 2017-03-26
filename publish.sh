c349ca92869ea7332e955b42f13b04a35489f7f2
Janosch Rohdewald <nordewal@gmail.com>
2017-03-26 11:53:10 +0200

    publish with one command

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rvm
 bundle exec rake publish
 #s3cmd -r --exclude="*small*" --exclude="*large*" sync source/blog/ s3://vonderwelt-source
 s3cmd -r -P --exclude=".git/*" --no-preserve --add-header='Cache-Control:max-age=31536000, public' --add-header="Expires:`date -u +"%a, %d %b %Y %H:%M:%S GMT" --date "next Year"`" sync build/ s3://vonderwelt
