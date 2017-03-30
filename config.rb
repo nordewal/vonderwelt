4c2f1305793d30424122219bc00d01da0f3d05a3
Janosch Rohdewald <nordewal@gmail.com>
2017-03-30 09:47:40 +0200

    case insensitive for asset host

--- config.rb
+++ config.rb
@@ -87,7 +87,7 @@ configure :build do
   ignore '*.css.map'
   ## Append a hash to asset urls (make sure to use the url helpers)
   activate :asset_hash,
-    :ignore => [/blog\/.*jpg$/i]
+    :ignore => [/blog\/.*(jpg|JPG)$/]
   activate :asset_host,
     :host => '//1bbd085e69c44879b4aea5ce2016ffff.ds11s3ns.swisscom.com/vonderwelt',
     :ignore => [/(js|css|ico|svg)$/]
